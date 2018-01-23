9405e6530bf0c2bbfcb0e2ebfd13a5e56ee576d6
motolola <motolola@icloud.com>
2018-01-23 19:22:41 +0000

    Teaching arrays ..

--- src/main/java/random/MyArray.java
+++ src/main/java/random/MyArray.java
@@ -1,5 +1,6 @@
 package random;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MyArray {
@@ -7,14 +8,54 @@ public class MyArray {
 	public static void main(String[] args) {
 		
 		System.out.println(Arrays.toString(myArr()));
-	
+		System.out.println(Arrays.toString(myIntArr()));
+		System.out.println(Arrays.toString(myIntegerArray()));
+		System.out.println(myList());
+		System.out.println(myStringList());
 	}
 	
 	public static String[] myArr() {
 		
-		String arr[] = new String[2];
+		String arr[] = new String[3];
 		arr[0] = "Zero";
 		arr[1] = "One";
+		arr[2] = "Two";
+		
+		return arr;	
+	}
+	
+	public static int[] myIntArr() {
+		int arr[] = new int[3];
+		arr[0] = 0;
+		arr[1] = 1;
+		arr[2] = 2;
+		
+		return arr;
+	}
+	
+	public static Integer[] myIntegerArray() {
+		Integer arr[] = new Integer[3];
+		arr[0] = 0;
+		arr[1] = 1;
+		arr[2] = 2;
+		
+		return arr;
+	}
+	
+	public static ArrayList<Integer> myList(){
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		arr.add(2);
+		arr.add(3);
+		arr.add(4);
+		
+		return arr;	
+	}
+	public static ArrayList<String> myStringList(){
+		ArrayList<String> arr = new ArrayList<String>();
+		arr.add("Zero");
+		arr.add("One");
+		arr.add("Two");
+		arr.add("Three");
 		
 		return arr;	
 	}
