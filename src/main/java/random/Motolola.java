39ceff2f63d6b462d6edc0a0ab39163536b95d6f
motolola <motolola@icloud.com>
2018-02-18 15:44:18 +0000

    Writing my name lol …

---
+++ src/main/java/random/Motolola.java
@@ -0,0 +1,20 @@
+package random;
+
+public class Motolola {
+
+	public static void main(String[] args) {
+		System.out.println("  ***                 ***        *********         *****************       *********");
+		System.out.println("  ****               ****       ************       *****************      ************");
+		System.out.println("  ******            *****      ***         ***            ***            ***         ***");
+		System.out.println("  *******          ******     ***           ***           ***           ***           ***");
+		System.out.println("  ***  ***        *** ***    ***             ***          ***          ***             ***");
+		System.out.println("  ***   ***      ***  ***    ***              ***         ***          ***             ***");
+		System.out.println("  ***    ***   ***    ***    ***              ***         ***          ***             ***");
+		System.out.println("  ***     *******     ***     ***            ***          ***           ***            ***");
+		System.out.println("  ***      *****      ***      ***           ***          ***            ***          ***");
+		System.out.println("  ***      *****      ***       **************            ***             **************");
+		System.out.println("  ***      *****      ***         *********               ***               *********");
+
+	}
+
+}
