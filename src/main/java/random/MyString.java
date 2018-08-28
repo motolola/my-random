59c44f59ce6a292a3ca783b32f70122f7d179330
motolola <motolola@icloud.com>
2018-08-28 19:57:32 +0100

    String stuff

---
+++ src/main/java/random/MyString.java
@@ -0,0 +1,19 @@
+package random;
+
+public class MyString {
+
+    public static void main(String[] args)
+    {
+        String str = "Boy";
+
+        System.out.println(str);
+        System.out.println(str.concat(" is good"));
+
+        System.out.println(str);
+
+        str = "Girl";
+
+        System.out.println(str);
+        System.out.println(str.concat(" is good"));
+    }
+}
