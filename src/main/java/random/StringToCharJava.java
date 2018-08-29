c47969982d666675fca455ba0a796c3f93c2bdbb
motolola <motolola@icloud.com>
2018-08-29 23:01:15 +0100

    CharArray

---
+++ src/main/java/random/StringToCharJava.java
@@ -0,0 +1,11 @@
+package random;
+
+public class StringToCharJava {
+
+    public static void main(String[] args)
+    {
+        String str = "Journalist";
+        char[] chars = str.toCharArray();
+        System.out.println(chars.length);
+    }
+}
