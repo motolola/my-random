2d593a05725c7bc364f95cf3958a3d8355d2d19d
motolola <motolola@icloud.com>
2018-09-08 19:16:16 +0100

    Exception Handling

---
+++ src/main/java/random/MyExceptionHandling.java
@@ -0,0 +1,35 @@
+package random;
+
+public class MyExceptionHandling {
+
+    public static void main(String[] args)
+    {
+        try {
+            method1();
+            int i = 1;
+            //i = i/0;
+        } catch (Exception e) {
+            System.out.println("Hello Catch!");
+        } finally {
+            System.out.println("This is finally");
+        }
+        method3(5,1);
+    }
+
+    public static void method1()
+    {
+        method2();
+    }
+    public static void method2()
+    {
+        System.out.println("I am method 2");
+    }
+    public static void method3(int a, int b)
+    {
+        if (b > a){
+            throw new RuntimeException("B is greater than A");
+        }
+        System.out.println(a-b);
+
+    }
+}
