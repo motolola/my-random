4451c315b7986a4c4aa7e52141795d2db68f1407
motolola <motolola@icloud.com>
2018-09-11 22:50:16 +0100

    Human stuff

--- src/main/java/random/MyVariableArgument.java
+++ src/main/java/random/MyVariableArgument.java
@@ -1,4 +1,8 @@
 package random;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
   variable arguments in Java.
  */
@@ -7,6 +11,10 @@ public class MyVariableArgument {
     public static void main(String[] args)
     {
         System.out.println(mySum(3,4,5,6, 8,9));
+        for (Human myHuman : myHumans()){
+            System.out.println(myHuman.toString());
+        }
+        //variableHuman(myHumans());
     }
 
     public static int mySum(int... numbers)
@@ -17,5 +25,67 @@ public class MyVariableArgument {
           sum += number;
         }
         return sum;
+    }
+
+    public static void variableHuman(Human... humans)
+    {
+        for(Human myHum : humans){
+            System.out.println("Human being: "+myHum.toString());
+        }
+    }
+
+    public static ArrayList<Human> myHumans()
+    {
+        ArrayList<Human> humanList = new ArrayList();
+
+        humanList.add(new Human(1,"Baba", 32));
+        humanList.add(new Human(2, "Suwe", 43));
+        humanList.add(new Human(3, "Olowo", 56));
+
+        return humanList;
+    }
+}
+class Human {
+    private int id;
+    private String name;
+    private int age;
+
+    public Human(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
