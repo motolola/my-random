ed942ffb213fedb335c4e35ad01d1df5598e6c1c
motolola <motolola@icloud.com>
2018-09-12 20:34:28 +0100

    More Lambda

--- src/main/java/random/functionalProgramming/MyLambdaEx.java
+++ src/main/java/random/functionalProgramming/MyLambdaEx.java
@@ -2,6 +2,7 @@ package random.functionalProgramming;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class MyLambdaEx {
 
@@ -17,10 +18,34 @@ public class MyLambdaEx {
 
         int mySum = numbers.stream()
                 .filter(num -> (num >= 5))
-                //map((n) -> n-2)
                 .reduce(0, Integer::sum);
         System.out.println(mySum);
 
         numbers.forEach(i -> System.out.println(i+" squared = "+i*i));
+
+        IntStream
+               .range(1,20)
+               .filter(i -> (i > 3))
+                .filter(m -> (m <= 11))
+               .filter(p -> (p%2 != 0))
+               .forEach(System.out::println);
+
+        System.out.println(IntStream
+                .range(1,5)
+                .filter(i -> i < 5)
+                .sum());
+
+        System.out.println("------ Squares of Ints ------");
+
+        Arrays.stream(new int[] {2,4,5,7,8,9})
+                .map(i -> i * i)
+                .forEach(System.out::println);
+
+        System.out.println("------ String Array Stream ------");
+
+        Arrays.stream(new String[] {"Apple", "Banana","Orange","Pineapple", "Papaya", "Pear", "Strawberry"})
+                .filter(s -> s.startsWith("P"))
+                .sorted()
+                .forEach(System.out::println);
     }
 }
