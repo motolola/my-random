0be9f11c39747960b1b8b771f038196198804743
Motolola <motolola@icloud.com>
2019-06-09 20:37:56 +0100

    Some updates.

--- src/main/java/com/motolola/practice/ThreadApp.java
+++ src/main/java/com/motolola/practice/ThreadApp.java
@@ -2,27 +2,30 @@ package com.motolola.practice;
 
 public class ThreadApp {
 	
-	public static void main(String[] ars) {
-		Runner runner1 = new Runner();
-		Runner runner2 = new Runner();
+	public static void main(final String[] ars)
+    {
+		final Runner runner1 = new Runner();
+		final Runner runner2 = new Runner();
+		final Runner runner3 = new Runner();
 		
 		runner1.start();
 		runner2.start();
-		
-		
+		runner3.start();
 	}
 
 }
 
-class Runner extends Thread {
+class Runner extends Thread
+{
 	
-	public void run() {
+	@Override
+    public void run() {
 		for (int i = 0; i <=10; i++) {
 			
 			System.out.println("Hello number "+i);
 			try {
 				Thread.sleep(100);
-			} catch (InterruptedException e) {
+			} catch (final InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
